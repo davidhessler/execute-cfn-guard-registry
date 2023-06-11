38c96f746e3ec048041f84c308bb2f5d9a8cfb40
David Hessler <dhhessl@amazon.com>
2023-06-11 08:40:12 -0400

    fix: updates rust install to reflect new install command

--- src/cfn-guard-rule-executor.ts
+++ src/cfn-guard-rule-executor.ts
@@ -10,7 +10,9 @@ export enum OutputFormat {
 
 export class CfnGuardRuleExecutor {
   async install(): Promise<void> {
-    await exec.exec('curl https://sh.rustup.rs -sSf | sh -s -- -y > /dev/null')
+    await exec.exec(
+      "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+    )
     await exec.exec('source "$HOME/.cargo/env" && cargo install cfn-guard')
     const ret = await exec.exec(
       'source "$HOME/.cargo/env" && cfn-guard --version'
